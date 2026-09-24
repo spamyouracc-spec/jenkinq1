3ad85e6fc2e59a8e80c1e4463e2c87c9727e9e5a
spamyouracc-spec <spamyouracc@gmail.com>
2026-09-24 10:25:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,6 @@
-from app import square, is_even
+def square(n):
+    return n * n
 
 
-def test_square_positive_number():
-    assert square(5) == 25
-
-
-def test_square_negative_number():
-    assert square(-4) == 16
-
-
-def test_is_even_even_number():
-    assert is_even(8) is True
-
-
-def test_is_even_odd_number():
-    assert is_even(7) is False
+def is_even(n):
+    return n % 2 != 0
